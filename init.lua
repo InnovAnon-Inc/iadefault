127d2043e3b541f8825948b76747ab44f4e55ed6
lmaddox <InnovAnon-Inc@protonmail.com>
2023-12-26 17:15:23 -0600

    initial

---
+++ init.lua
@@ -0,0 +1,8 @@
+local MODNAME = minetest.get_current_modname()
+local S       = minetest.get_translator(MODNAME)
+local path    = minetest.get_modpath(MODNAME).."/"
+
+-- Global
+iadefault = {}
+
+print ("[MOD] IA Default loaded")
